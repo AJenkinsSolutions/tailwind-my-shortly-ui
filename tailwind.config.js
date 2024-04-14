afba827daf4f3c0763bf56bc9239bcbcf339a096
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-04-14 11:14:04 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+
+    },
+    extend: {
+      fontFamily: {
+
+      },
+      colors: {
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
